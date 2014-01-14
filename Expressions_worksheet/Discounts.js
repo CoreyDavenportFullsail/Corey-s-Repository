b1d9d5589b9d8d255620cd7f97515b0068294c68
CoreyDavenportFullsail <croireng@gmail.com>
2014-01-14 03:03:14 -0500

    Added original price variable

--- Expressions_worksheet/Discounts.js
+++ Expressions_worksheet/Discounts.js
@@ -1,3 +1,6 @@
 /**
- * Created by Administrator on 1/14/14.
+ * Created by Corey Davenport on 1/13/14. "Discounts"
  */
+//Purpose of this program is to display price of discounted item, before and after sales tax
+
+var originalPrice= 100;
