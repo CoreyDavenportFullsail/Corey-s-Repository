cc8b585432ba3d8c819dc4f71ff936e97249ba0b
CoreyDavenportFullsail <croireng@gmail.com>
2014-01-13 23:55:32 -0500

    Wrote entire program
    Defined amount of pizzas at party, number of slices per pizza, number
    of people at party, and how many slices each person gets.

--- Expressions_worksheet/Slice Of Pie Part 1.js
+++ Expressions_worksheet/Slice Of Pie Part 1.js
@@ -1,4 +1,12 @@
 //Created by Corey Davenport on 1/13/14. Slice of Pie Part 1
+// The purpose of this is to accuratel calculate how many slices of pizza 
+//  everyone will receive at a party.
 
-//
+var pizzasOrdered= 6;
+var slicesPerPizza= 8;
+var peopleAtParty= 12;
+var totalSlices= pizzasOrdered*slicesPerPizza;
+var slicesPerPerson= totalslices/peopleAtParty;
 
+console.log("Each person ate" + " " + slicesPerPerson + " " + "slices of pizza at the party"
+             );
