1c36f455af79c3f66f0c3e62790c62f05a3f8cfa
CoreyDavenportFullsail <croireng@gmail.com>
2014-01-14 02:29:37 -0500

    Created variable amountSpent

--- Expressions_worksheet/Average_Shopping_Bill.js
+++ Expressions_worksheet/Average_Shopping_Bill.js
@@ -1,3 +1,7 @@
 /**
- * Created by Administrator on 1/14/14.
+ * Created by Corey Davenport on 1/14/14. "Average Shopping Bill"
  */
+//This program is intended to show how much you have spent on groceries within the past 5 weeks, as well as the
+//average amount of $ spent.
+
+var amountSpent= [234,189,264,203,212];
