03945cc3194598d6a2dc52d148eedb52f12f3d7d
CoreyDavenportFullsail <croireng@gmail.com>
2014-01-14 01:04:14 -0500

    Added both files, completed
    For some reason, as I was coding, Github wasn't updating and allowing
    me to commit changes I made. So this is the completed code.

--- Expressions_worksheet/Slice_Of_Pie_Part_2.js
+++ Expressions_worksheet/Slice_Of_Pie_Part_2.js
@@ -3,3 +3,14 @@
  */
 // The purpose of this program will be to print out the number of slices left over for Sparky to eat after all
 //the pizza slices have been divided up, using the same numbers from "Slice of Pie Part 1"
+
+//First we need to sort out how many pieces each person gets
+
+var pizzasOrdered= 6;
+var slicesPerPizza= 8;
+var peopleAtParty= 12;
+var slicesTotal= pizzasOrdered*slicesPerPizza;
+var slicesPerPerson= slicesTotal/peopleAtParty;
+var slicesSparkyGets= slicesTotal % peopleAtParty;
+
+console.log("Sparky got" + " " + slicesSparkyGets + " " + "slices of pizza.");
