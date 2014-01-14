741c050bb264a0ffbf43396f33206791eeb1be4e
CoreyDavenportFullsail <croireng@gmail.com>
2014-01-14 00:46:12 -0500

    Added Slice of Pie Pt 2 .js and index file
    Also added some comments in the .js file

---
+++ Expressions_worksheet/Slice_Of_Pie_Part_2.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Corey Davenport on 1/14/14. "Slice of Pie Part 2"
+ */
+// The purpose of this program will be to print out the number of slices left over for Sparky to eat after all
+//the pizza slices have been divided up, using the same numbers from "Slice of Pie Part 1"
