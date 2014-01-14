dca1e239a3700df180009fe2fc19686a1cb0258f
CoreyDavenportFullsail <croireng@gmail.com>
2014-01-13 22:52:55 -0500

    Created .js file and added comments of intent
    created .js file and index.html and stated the intent of the program
    through comments

---
+++ Expressions_worksheet/Sparkys_Age_Calculator.js
@@ -0,0 +1,8 @@
+
+// * Created by Corey Davenport on 1/13/14. "Sparky's Age Calculator"
+
+//Objective: Because dogs age 7 times faster than humans, their "age" is generally considered to be their actual age
+//multiplied by 7. This program will calculate "dog age".
+
+
+
