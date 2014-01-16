fb279bc833e06bed0c3d420677acbeb02a89ef16
CoreyDavenportFullsail <croireng@gmail.com>
2014-01-15 22:11:25 -0500

    added givens

--- Celsius To Ferhenheit/Celsius_To_Ferhenheit.js
+++ Celsius To Ferhenheit/Celsius_To_Ferhenheit.js
@@ -1,4 +1,8 @@
 /**
- * Created by Corey Davenport on 1/15/14. "Celsius To Ferhenheit"
+ * Created by Corey Davenport on 1/15/14. "Celsius To Farhenheit"
  */
-//Purpose is to convert a temperature to degrees Celsius or degrees Ferhenheit
+//Purpose is to convert a temperature to degrees Celsius or degrees Farhenheit
+
+var temp=prompt("What is the temperature?");
+var unit=prompt("Type 'C' if Celsius, or 'F' for Farhenheit.");
+
