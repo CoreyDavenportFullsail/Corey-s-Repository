bff5383489a2c1b6b99a0cf34727e47c11ebc45b
CoreyDavenportFullsail <croireng@gmail.com>
2014-01-15 21:31:16 -0500

    added variables and first if statement

--- Conditional Worksheet/Stuff_Your_Face_1.js
+++ Conditional Worksheet/Stuff_Your_Face_1.js
@@ -1,3 +1,12 @@
 /**
- * Created by Administrator on 1/15/14.
+ * Created by Corey Davenport on 1/15/14. "Stuff Your Face 1"
  */
+// The purpose of this program is to tell whether or not a competitor meets the weight requirement in order to
+//   enter a pie eating contest
+
+var competitorWeight= 267;
+var weightRequirement= 250;
+
+if(competitorWeight >= weightRequirement){
+    console.log("The competitor qualifies for the heavyweight division.");
+}
