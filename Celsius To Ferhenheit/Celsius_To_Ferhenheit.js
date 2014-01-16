bc7462e570051389769c30b6d907e3e2a33227ae
CoreyDavenportFullsail <croireng@gmail.com>
2014-01-15 22:21:24 -0500

    Included a warning about case sensitivity

--- Celsius To Ferhenheit/Celsius_To_Ferhenheit.js
+++ Celsius To Ferhenheit/Celsius_To_Ferhenheit.js
@@ -4,7 +4,7 @@
 //Purpose is to convert a temperature to degrees Celsius or degrees Farhenheit
 
 var temp=prompt("What is the temperature?");
-var unit=prompt("Type 'C' if Celsius, or 'F' for Farhenheit.");
+var unit=prompt("Type 'C' if Celsius, or 'F' for Farhenheit.THIS IS CASE SENSITIVE.");
 
 if(unit === "F"){
     alert((temp-32)/1.8 + " degrees Celsius")
