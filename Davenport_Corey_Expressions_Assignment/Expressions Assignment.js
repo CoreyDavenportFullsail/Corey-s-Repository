168395bbcf1b22a549cd81a7fb34045c1cebff87
CoreyDavenportFullsail <croireng@gmail.com>
2014-01-19 17:44:21 -0500

    Improved alert statements

--- Davenport_Corey_Expressions_Assignment/Expressions Assignment.js
+++ Davenport_Corey_Expressions_Assignment/Expressions Assignment.js
@@ -4,16 +4,17 @@
 
 //Personal
 //"Area and Perimeter of a Right Triangle Calculator"
-//This program is intended for personal use to help find the area of a right triangle.
+    //This program is intended for personal use to help find the area of a right triangle, because I'm in a math.
+    //class at the moment.
 
 var b=prompt("What is the measurement of the base?");
 var h=prompt("What is the measurement of the height?");
 var area= 1/2*parseFloat(b)*parseFloat(h);
-var areaAlert=alert("The area of the triangle is "+ area);
-var nextValue=alert("Now to find the perimeter...");
+alert("The area of the triangle is "+ area);
+alert("Now to find the perimeter...");
 var sideC=prompt("What is the third measurement of the triangle?");
 var perimeter= parseFloat(b)+parseFloat(h)+parseFloat(sideC);
-var perimeterAlert=alert("The perimeter of the triangle is "+perimeter);
+alert("The perimeter of the triangle is "+perimeter);
 
 
 //Industry
