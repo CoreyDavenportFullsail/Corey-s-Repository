2ca433b6ef02890d89e1eaa294e457ebf530fd27
CoreyDavenportFullsail <croireng@gmail.com>
2014-01-19 17:26:44 -0500

    added perimeter variable

--- Davenport_Corey_Expressions_Assignment/Expressions Assignment.js
+++ Davenport_Corey_Expressions_Assignment/Expressions Assignment.js
@@ -3,13 +3,17 @@
  */
 
 //Personal
-//"Area of a Right Triangle Calculator"
+//"Area and Perimeter of a Right Triangle Calculator"
 //This program is intended for personal use to help find the area of a right triangle.
 
 var b=prompt("What is the measurement of the base?");
 var h=prompt("What is the measurement of the height?");
 var area= 1/2*parseFloat(b)*parseFloat(h);
-var areaPrompt=alert("The area of the triangle is "+ area);
+var areaAlert=alert("The area of the triangle is "+ area);
+var nextValue=alert("Now to find the perimeter...");
+var sideC=("What is the third measurement of the triangle?");
+var perimeter= b+h+sideC;
+alert("The perimeter of the triangle is "+perimeter);
 
 
 //Industry
