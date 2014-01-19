8e324df5c1941f41e235e282e62b2c730a6cd26a
CoreyDavenportFullsail <croireng@gmail.com>
2014-01-19 16:37:37 -0500

    fixing problem

--- Davenport_Corey_Expressions_Assignment/Expressions Assignment.js
+++ Davenport_Corey_Expressions_Assignment/Expressions Assignment.js
@@ -5,14 +5,3 @@
 //Personal
 //"Area of a Right Triangle Calculator"
 //This program is intended for personal use to help find the area of a right triangle.
-
-//Would have made a commit here
-
-var b=prompt("What is the measurement of the base?");
-//commit here: made base variable prompt
-var h=prompt("What is the measurement of the height?");
-//commit here: made height variable prompt
-var area= 1/2*b*h;
-//commit here: made area variable
-var areaPrompt=alert("The area of the triangle is "+ area);
-//commit here: made an alert that tells the user the area of the triangle
