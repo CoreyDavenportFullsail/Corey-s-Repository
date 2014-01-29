d8741b8eb8976eb75ad40e5114e69a34ac9a7b0a
CoreyDavenportFullsail <croireng@gmail.com>
2014-01-28 22:18:52 -0500

    added alert

--- Davenport_Corey_Conditionals/Conditionals.js
+++ Davenport_Corey_Conditionals/Conditionals.js
@@ -1,3 +1,11 @@
 /**
  * Created by Corey Davenport on 1/28/14. "Conditionals Assignment"
  */
+
+//Personal
+// "Are you going to need a sweater?"
+// This program is intended to tell me, based on a couple weather conditions, whether or not I am going to need
+// a sweater.
+alert("Are you going to need a sweater when you go out?");
+
+
