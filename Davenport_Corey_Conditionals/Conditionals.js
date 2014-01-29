eb94a3797cbdb7d08cac336141c90756b4fcf3d9
CoreyDavenportFullsail <croireng@gmail.com>
2014-01-28 22:20:36 -0500

    added else statement

--- Davenport_Corey_Conditionals/Conditionals.js
+++ Davenport_Corey_Conditionals/Conditionals.js
@@ -12,5 +12,7 @@ alert("Are you going to need a sweater when you go out?");
 var userTemperature=prompt("What is the temperature?");
 
 if(parseInt(userTemperature)>=70){
-    alert("Have fun! You're not going to need a sweater!")
+    alert("Have fun! You're not going to need a sweater!");
+}else{
+    alert("Go get a sweater.");
 }
