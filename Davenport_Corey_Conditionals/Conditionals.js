40264d3e0b48776927fe79b87fd8b7bb76a618fc
CoreyDavenportFullsail <croireng@gmail.com>
2014-01-28 22:47:13 -0500

    added terribleness variable

--- Davenport_Corey_Conditionals/Conditionals.js
+++ Davenport_Corey_Conditionals/Conditionals.js
@@ -42,3 +42,5 @@ alert("Should you hit on them?");
 var theirAttractiveness=prompt("Score the subject on a scale of 1-10 on their attractiveness.");
 var yourAttractiveness=prompt("Score yourself on a scale of 1-10 on your attractiveness...BE HONEST.");
 var theirRelationshipStatus=prompt("What is their relationship stauts? Answer Single, Taken, or Unsure. CASE SENSITIVE);
+var yourRelationshipStatus=prompt("What is your relationship status? Answer Single, Taken, or Unsure. CASE SENSITIVE");
+var terribleness=prompt("Are you a terrible person who would cheat? Answer y for Yes, or n for No");
