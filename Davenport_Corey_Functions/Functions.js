ed124c676dd738b1d3dda723fe8d515df869529a
CoreyDavenportFullsail <croireng@gmail.com>
2014-02-02 18:35:17 -0500

    added alert

--- Davenport_Corey_Functions/Functions.js
+++ Davenport_Corey_Functions/Functions.js
@@ -1,3 +1,8 @@
 /**
  * Created by Corey Davenport on 2/2/14.
  */
+//Personal
+//"Where to eat" This program is just going to tell me based on the amount of money I have where I should go out to eat
+
+alert("Where should you go ouy yo eat?");
+
